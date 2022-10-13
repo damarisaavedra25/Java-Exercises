a4f4e157b786ce5158a080e1047bf17ad6bf4c08
damaris0522 <damaris.saavedra@dexcom.com>
2022-10-13 14:48:52 -0700

    java exercises

---
+++ Ch03/03_02/Arguments/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello from Java!");
+		System.out.println(args[0]);
+	}
+
+}dmdmd dmdmdmdd dfjsdnn da dmad ma  fmd fmdf mdmd am anfdmndsjadsnkac ddmdmasmdmdamdamdmdma v smd dmda sdm dcam fmdmdmdmdmmd md dmadmad mad md smmdmdmdmdmdmdsm dm md dmd md dmd dmdmdmdmdmd dmdmd  dmd dmd dmd mdmd dmddm dmd dmdmd dmd dmd md dmd md dmdmddmdmd dma dmad amd admd dmd md md dm dmd 
---
+++ Ch04/04_02/PrimitiveNumbers/src/com/example/java/Main.java
@@ -0,0 +1,23 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+    byte b = 1;
+    short sh = 1;
+    int i = 1;
+    long l = 1L;
+
+    float f = 1f;
+    double d = 1d;
+        System.out.println("Byte: " + b);
+
+        System.out.println("Byte: " + sh);
+        System.out.println("Short: " + i);
+        System.out.println("Int: " + l);
+        System.out.println("Long: " + f);
+        System.out.println("Float: " + d);
+
+    }
+
+}
